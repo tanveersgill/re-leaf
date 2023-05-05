001441e5cc06ea1ca330e72a6526f6ea92ed6ca2
Jason Kuo <89288700+jkuo630@users.noreply.github.com>
2023-05-05 16:06:10 -0700

    Update Footer.jsx
    copyright change and name change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { AiFillInstagram } from "react-icons/ai";
 export default function Footer() {
   return (
     <FooterContainer>
-      <span>Copyright &copy; 2021 Travelo. All rights reserved</span>
+      <span>Copyright &copy; 2023 Re-Leaf. All rights reserved</span>
       <ul className="links">
         <li>
           <a href="#hero">Home</a>
