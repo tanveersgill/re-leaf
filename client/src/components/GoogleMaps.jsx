41135793ad638157da60fe7d65a21f0501252928
jkuo630 <89288700+jkuo630@users.noreply.github.com>
2023-05-05 21:25:01 -0400

    google maps resizing + formating

--- client/src/components/GoogleMaps.jsx
+++ client/src/components/GoogleMaps.jsx
@@ -1,14 +1,23 @@
 const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY;
 
+console.log(GOOGLE_MAPS_API_KEY);
+
 const GoogleMaps = () => {
   return (
-    <iframe
-      title="Google Maps"
-      src={`https://www.google.com/maps/embed/v1/directions?key=${GOOGLE_MAPS_API_KEY}
-    &origin=Toronto
-  &destination=SanFrancisco+California
-  &mode=flying`}
-    />
+    <div className="pageOne">
+      <div className="map">
+        <iframe
+          width="1200"
+          height="825"
+          allowfullscreen
+          title="Google Maps"
+          src={`https://www.google.com/maps/embed/v1/directions?key=${GOOGLE_MAPS_API_KEY}
+          &origin=Toronto
+          &destination=SanFrancisco+California
+          &mode=flying`}
+        />
+      </div>
+    </div>
   );
 };
 
