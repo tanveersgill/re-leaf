276b6efd26e3ddfb96c4cd18e9032e44dcc2b1e5
jkuo630 <89288700+jkuo630@users.noreply.github.com>
2023-05-05 20:39:34 -0400

    testimonial change

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import avatarImage from "../assets/avatarImage.jpeg";
 import jasonKuo from "../assets/jasonkuo.jpeg";
 import williamWong from "../assets/williamwong.jpeg";
 import kaiThompson from "../assets/kaithompson.jpeg";
