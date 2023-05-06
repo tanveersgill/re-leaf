6c1aa48779d71c021f4c9d90fabae536f7b990c0
kai-thompson <kai@newton.co>
2023-05-05 21:50:50 -0400

    add auth0 login/logout

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Auth0Provider
+    domain={import.meta.env.VITE_APP_AUTH0_DOMAIN}
+    clientId={import.meta.env.VITE_APP_AUTH0_CLIENT_ID}
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
-)
+  </Auth0Provider>
+);
